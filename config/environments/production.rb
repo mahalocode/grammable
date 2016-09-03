f28ed86c5b2ca1edcd2ff563ea2c2e3c023f1997
mahalocode <you@example.com>
2016-09-03 07:13:04 +0000

    typo error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_mailer.default_url_option = {host: 'grammable-tom-y.herokuapp.com' }
+  config.action_mailer.default_url_options = {host: 'grammable-tom-y.herokuapp.com' }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
