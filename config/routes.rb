3f4b15f793d73d2682a96cb47643df9e2fa1c9df
mahalocode <you@example.com>
2016-09-21 06:57:04 +0000

    run migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "grams#index"
   resources :grams do 
-    resources :comments, only: :create
+    resources :comments, only: :create 
   end
 end
   # The priority is based upon order of creation: first created -> highest priority.
